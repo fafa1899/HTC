399887f3517de453ba982547f3f92d907f370fe1
fafa1899 <holybell44@126.com>
2020-01-24 19:18:54 +0800

    1.0

---
+++ TaskProgram/TaskProgram/TaskProgram.cpp
@@ -0,0 +1,50 @@
+﻿// TaskProgram.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <time.h>   
+
+using namespace std;
+
+int main()
+{
+	fprintf(stdout, "开始运行\n");
+
+	//延时10S
+	fprintf(stdout, "延时10S\n");
+	time_t first = time(NULL);
+	double diff = 0;
+	while (diff<10)
+	{
+		time_t second = time(NULL);
+		diff = difftime(second, first);			//计时		
+	}
+
+	ifstream infile("input.txt");
+	if (!infile)
+	{
+		fprintf(stderr, "无法读取文件\n");
+		return 1;
+	}
+
+	string line;
+	getline(infile, line);
+	
+
+	ofstream outfile("output.dat");
+	if (!outfile)
+	{
+		fprintf(stderr, "无法写出文件\n");
+		return 1;
+	}
+
+	outfile << "输出内容:\n";
+	outfile << line;
+
+	fprintf(stdout, "运行完成\n");
+
+	return 0;
+}
+
---
+++ Work/del.bat
@@ -0,0 +1,4 @@
+del D:\Work\HTC\Work\*.log /f /s
+del D:\Work\HTC\Work\*.error /f /s
+del D:\Work\HTC\Work\*.dat /f /s
+del D:\Work\HTC\Work\*.out /f /s
